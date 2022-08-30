c095a13d0cc67e9fdc1422f72371764add94e4c4
Summer Lee <lee@summer.today>
2022-08-31 04:45:21 +0800

    complete part10/10.11

---
+++ src/graphql/queries.js
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+export const GET_REPOSITORIES = gql`
+	query GetRepositories {
+		repositories {
+			edges {
+				node {
+					id
+					name
+					fullName
+					ratingAverage
+					reviewCount
+					stargazersCount
+					forksCount
+					ownerAvatarUrl
+					description
+					language
+				}
+			}
+		}
+	}
+`;
