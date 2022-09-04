9511f8c0bee2089df96a9f091db2b6df43b24d55
Summer Lee <lee@summer.today>
2022-09-04 12:41:54 +0800

    generate Android App preview

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 export default {
-	name: 'rate-repository-app',
+	name: 'Rate repository',
 	slug: 'rate-repository-app',
 	version: '1.0.0',
 	orientation: 'portrait',
@@ -24,6 +24,7 @@ export default {
 			foregroundImage: './assets/adaptive-icon.png',
 			backgroundColor: '#FFFFFF',
 		},
+		package: 'com.summerlee.rate_repository_app'
 	},
 	web: {
 		favicon: './assets/favicon.png',
@@ -31,5 +32,8 @@ export default {
 	extra: {
 		env: process.env.ENV,
 		apolloUri: process.env.APOLLO_URI,
+		eas: {
+			projectId: 'e41194e5-7140-4e60-a294-de47a0932491',
+		},
 	},
 };
